2a254d0664d63023be203dfb6de6f853bbd0ccbb
Omg-A <67595078+Omg-A@users.noreply.github.com>
2022-02-10 18:20:11 +0300

    Add files via upload

--- html ide.py
+++ html ide.py
@@ -1,7 +1,9 @@
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import filedialog
+import tkinter.messagebox as tkmb
 import os
+import webbrowser
 
 root = Tk()
 root.title("HTML file")
@@ -9,10 +11,10 @@ root.minsize(650,650)
 root.maxsize(650,650)
 
 label_name = Label(root, text="Your file name:")
-label_name.place(relx=0.28, rely=0.03, anchor=CENTER)
+label_name.place(relx=0.36, rely=0.03, anchor=CENTER)
 
 input_name = Entry(root)
-input_name.place(relx=0.46, rely=0.03, anchor=CENTER)
+input_name.place(relx=0.55, rely=0.03, anchor=CENTER)
 
 my_text = Text(root, width=80, height=35, bg="light slate gray")
 my_text.place(relx=0.5, rely=0.55, anchor=CENTER)
@@ -30,12 +32,30 @@ def openFile():
     input_name.insert(END, formated_name)
     root.title(formated_name)
     html_file = open(name, "r")
-    paragraph = text_file.read()
-    my_html.insert(END, paragraph)
+    paragraph = html_file.read()
+    my_text.insert(END, paragraph)
     html_file.close()
+
+def save():
+    input_file_name = input_name.get()
+    file = open(input_file_name + ".html", "w")
+    data = my_text.get("1.0", END)
+    file.write(data)
+    input_name.delete(0, END)
+    my_text.delete(1.0, END)
+    tkmb.showInfo("Update", "HTML file has been saved")
+
+def run():
+    global name
+    webbrowser.open(name)
 
 open_button = Button(root, text="Open file", command=openFile)
 open_button.place(relx=0.05, rely=0.03, anchor=CENTER)
 
+save_button = Button(root, text="Save file", command=save)
+save_button.place(relx=0.15, rely=0.03, anchor=CENTER)
+
+run_button = Button(root, text="Run file", command=run)
+run_button.place(relx=0.24, rely=0.03, anchor=CENTER)
 
 root.mainloop()
